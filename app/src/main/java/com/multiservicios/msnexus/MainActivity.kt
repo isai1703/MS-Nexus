e7fa344412237c144514e7982b1446a8715d1e19
isai1703 <oscar_isai_170596@hotmail.com>
2026-08-05 14:53:13 -0600

    Corrige tema AppCompat y diagnóstico de MainActivity

--- app/src/main/java/com/multiservicios/msnexus/MainActivity.kt
+++ app/src/main/java/com/multiservicios/msnexus/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.multiservicios.msnexus
 
 import android.os.Bundle
+import android.graphics.Color
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.multiservicios.msnexus.databinding.ActivityMainBinding
 
@@ -11,7 +13,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        try {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+        } catch (e: Throwable) {
+
+            val error = TextView(this).apply {
+                text = "MS NEXUS - ERROR\n\n${e.javaClass.name}\n\n${e.message}\n\n${e.stackTraceToString()}"
+                textSize = 14f
+                setTextColor(Color.RED)
+                setPadding(32, 32, 32, 32)
+            }
+
+            setContentView(error)
+        }
     }
 }
