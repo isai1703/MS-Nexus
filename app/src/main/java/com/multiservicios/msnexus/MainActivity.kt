d8de268322ef19db607e6547b2f9394d583f7fd8
isai1703 <oscar_isai_170596@hotmail.com>
2026-08-10 01:21:32 -0600

    Prueba de arranque directo de MainActivity

--- app/src/main/java/com/multiservicios/msnexus/MainActivity.kt
+++ app/src/main/java/com/multiservicios/msnexus/MainActivity.kt
@@ -1,31 +1,22 @@
 package com.multiservicios.msnexus
 
-import android.os.Bundle
 import android.graphics.Color
+import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.multiservicios.msnexus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        try {
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-        } catch (e: Throwable) {
-
-            val error = TextView(this).apply {
-                text = "MS NEXUS - ERROR\n\n${e.javaClass.name}\n\n${e.message}\n\n${e.stackTraceToString()}"
-                textSize = 14f
-                setTextColor(Color.RED)
-                setPadding(32, 32, 32, 32)
-            }
-
-            setContentView(error)
+        val textView = TextView(this).apply {
+            text = "MS NEXUS\n\nMainActivity funcionando correctamente"
+            textSize = 24f
+            setTextColor(Color.BLACK)
+            setPadding(48, 48, 48, 48)
         }
+
+        setContentView(textView)
     }
 }
